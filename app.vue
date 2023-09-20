920ef64e46e2f14241deff302aa1afaed077939a
xfxpositions <96339770+xfxpositions@users.noreply.github.com>
2023-09-20 16:20:40 +0300

    scrollbar get hidden

--- app.vue
+++ app.vue
@@ -7,12 +7,7 @@
     </div>
   </div>
 </template>
-<style>
-.background {
-  background-image: url("/bg.jpg");
-  object-fit: contain;
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-}
-</style>
+<script setup>
+import "@/assets/css/main.css";
+</script>
+<style></style>
