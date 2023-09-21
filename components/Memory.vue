a0c0b678f97000ec2d363f00e878c0b89974f8bc
xfxpositions <96339770+xfxpositions@users.noreply.github.com>
2023-09-21 12:13:03 +0300

    Memory buttons added

---
+++ components/Memory.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="backdrop-blur-[24px] text-white bg-slate-50 bg-opacity-10 box-border py-3 px-4 select-none"
+  >
+    <div class="flex justify-around">
+      <Button
+        class="bg-slate-500 border-0 hover:bg-slate-600 box-border px-1 rounded-md bg-opacity-20"
+        >MC</Button
+      >
+      <Button
+        class="bg-slate-500 border-0 hover:bg-slate-600 box-border px-1 rounded-md bg-opacity-20"
+        >MR</Button
+      >
+      <Button
+        class="bg-slate-500 border-0 hover:bg-slate-600 box-border px-1 rounded-md bg-opacity-20"
+        >M+</Button
+      >
+      <Button
+        class="bg-slate-500 border-0 hover:bg-slate-600 box-border px-1 rounded-md bg-opacity-20"
+        >M-</Button
+      >
+      <Button
+        class="bg-slate-500 border-0 hover:bg-slate-600 box-border px-1 rounded-md bg-opacity-20"
+        >MS</Button
+      >
+    </div>
+  </div>
+</template>
